6d75f1edd5903e57cf2827fdbfe95624b65b5d23
ewilson294 <ecwil2@yahoo.com>
2021-11-25 14:42:16 -0600

    Create function to make series IDs

--- BLS_Extraction.R
+++ BLS_Extraction.R
@@ -1,10 +1,31 @@
+# Author: Eric Wilson
+# Date Created: 15 November 2021
+
 # This is initial, messy code to extract data from the BLS API
 
 library(blsAPI)
 library(rjson)
 
+blsSeriesIDs <- function(area_code, seasonal_adjustment = 'U', measure = '03'){
+    "seasonal_adjustment <- {'U', 'S}, where 'U' is unadjusted and 'S' is adjusted
+     measure <- {'03', '04', '05', '06'} where '03' is Unemployment Rate,
+        '04' is Unemployment, '05' is Employment, and '06' is Labor Force
+     area_code refers to BLS area codes
+    "
+    seriesID <- paste('LA', seasonal_adjustment, area_code, measure, sep = "")
+    return(seriesID)
+}
+
 # Load in the state codes
 state_codes <- read.table('./BLS DATA/sm.state.txt', sep ='\t', header = TRUE, colClasses = "character")
+area_file <- readLines('BLS DATA/la.area.txt')
+area_file <- gsub('([A-Z])\t[(A-Z0-9]{15}) (.+) ([0-9]+)\t([TF])\t([0-9]+)', "\\1'\\2'\\3'\\4'\\5'\\6'", area_file)
+tc <- textConnection(area_file)
+area_codes <- read.table(tc, sep = '\t', header = TRUE, colClasses = "character", quote = "")
+close(tc)
+
+# Create DataFrame of unemployment rate
+state_and_territories <- blsSeriesIDs(area_codes$area_code[area_codes$area_type_code == 'A'])
 
 # Example: Alabama Unemployment Rate, most recent 36 months
-alabama_unemployment_rate <- blsAPI(payload = 'LAUST010000000000003', return_data_frame = TRUE)
+# alabama_unemployment_rate <- blsAPI(payload = 'LAUST010000000000003', return_data_frame = TRUE)
